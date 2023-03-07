f8f10bdd49f8d325240f60fa983a19dbbe28aa99
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-07 22:49:28 +0200

    Add main Hilt configuration

---
+++ app/src/main/java/com/pavelhnelycia/test_compouse/app/ConsumeApiJetpackCompose.kt
@@ -0,0 +1,12 @@
+package com.pavelhnelycia.test_compouse.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * ConsumeApiJetpackCompose class with @HiltAndroidApp, which indicates that the application
+ * uses the Hilt dependency injection framework for Android.
+ */
+@HiltAndroidApp
+class ConsumeApiJetpackCompose : Application() {
+}
